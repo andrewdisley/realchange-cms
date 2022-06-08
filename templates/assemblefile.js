b65bec945fff41672a729c838a6be5d2a1d99088
Andrew Disley <andrew@simplified.co.uk>
2022-06-08 06:44:24 +0100

    Updates dependences to squish dependabot PRs

--- templates/assemblefile.js
+++ templates/assemblefile.js
@@ -1,90 +1,86 @@
-
-var autoprefixer = require('gulp-autoprefixer');
-var assemble = require('assemble');
-var browserSync = require('browser-sync').create();
-var concat = require('gulp-concat');
-var del = require('del');
-var extname = require('gulp-extname');
+var autoprefixer = require("gulp-autoprefixer");
+var assemble = require("assemble");
+var browserSync = require("browser-sync").create();
+var concat = require("gulp-concat");
+var del = require("del");
+var extname = require("gulp-extname");
 var include = require("gulp-include");
-var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
-var watch = require( 'base-watch' );
-var uglify = require('gulp-uglify');
+var jshint = require("gulp-jshint");
+var sass = require("gulp-sass")(require("node-sass"));
+var watch = require("base-watch");
+var uglify = require("gulp-uglify");
 
 var app = assemble();
 
-app.use( watch() );
+app.use(watch());
 
 // Config
 //
 var config = {
-  "project" : {
-    "shortname" : "realchange" // Used to name generated css and javascript
+  project: {
+    shortname: "realchange", // Used to name generated css and javascript
   },
-  "paths" : {
-    "src" : {
+  paths: {
+    src: {
       html: [
-        'src/_components/**/*.hbs',
-        'src/_layouts/*.hbs',
-        'src/pages/*.hbs',
-        'src/pages/**/*.hbs'
+        "src/_components/**/*.hbs",
+        "src/_layouts/*.hbs",
+        "src/pages/*.hbs",
+        "src/pages/**/*.hbs",
       ],
-      assets: [
-        'src/assets/**'
-      ],
+      assets: ["src/assets/**"],
       css: [
-        'src/_components/**/*.scss',
-        'src/css/sass/*.scss',
-        'src/css/sass/**/*.scss'
+        "src/_components/**/*.scss",
+        "src/css/sass/*.scss",
+        "src/css/sass/**/*.scss",
       ],
-      fonts: [
-        'src/fonts/**'
-      ],
-      js: [
-        'src/js/**'
-      ]
+      fonts: ["src/fonts/**"],
+      js: ["src/js/**"],
     },
-    "dist" : {
-      "html": "dist",
-      "assets" : "dist/assets",
-      "css" : "dist",
-      "js" : "dist/assets"
+    dist: {
+      html: "dist",
+      assets: "dist/assets",
+      css: "dist",
+      js: "dist/assets",
     },
-    "server": "./dist"
-  }
+    server: "./dist",
+  },
 };
 
 // Clean
 //
-app.task('clean', function() {
-  return del('dist/*');
+app.task("clean", function () {
+  return del("dist/*");
 });
 
 // Fonts
 //
-app.task('assets', function() {
-  return app.src(config.paths.src.assets)
+app.task("assets", function () {
+  return app
+    .src(config.paths.src.assets)
     .pipe(app.dest(config.paths.dist.assets));
 });
 
 // SASS
 //
-app.task('sass:main', function() {
-
-  return app.src('src/css/sass/main.scss')
-    // .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
-    .pipe(concat(config.project.shortname + '.css'))
-    .pipe(autoprefixer({ cascade: false }))
-    .pipe(app.dest(config.paths.dist.css))
-    .pipe(browserSync.stream());
+app.task("sass:main", function () {
+  return (
+    app
+      .src("src/css/sass/main.scss")
+      // .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+      .pipe(sass({ outputStyle: "compact" }).on("error", sass.logError))
+      .pipe(concat(config.project.shortname + ".css"))
+      .pipe(autoprefixer({ cascade: false }))
+      .pipe(app.dest(config.paths.dist.css))
+      .pipe(browserSync.stream())
+  );
 });
 
-app.task('sass:styleguide', function() {
-
-  return app.src('src/css/sass/styleguide.scss')
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(concat(config.project.shortname + '.styleguide.css'))
+app.task("sass:styleguide", function () {
+  return app
+    .src("src/css/sass/styleguide.scss")
+    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+    .pipe(concat(config.project.shortname + ".styleguide.css"))
     .pipe(autoprefixer({ cascade: false }))
     .pipe(app.dest(config.paths.dist.css))
     .pipe(browserSync.stream());
@@ -92,19 +88,19 @@ app.task('sass:styleguide', function() {
 
 // HTML
 //
-app.task('html', function() {
-
-  handlebars = require('handlebars');
-  helpers = require('handlebars-helpers')({
-    handlebars: handlebars
+app.task("html", function () {
+  handlebars = require("handlebars");
+  helpers = require("handlebars-helpers")({
+    handlebars: handlebars,
   });
 
-  app.partials(['src/_components/**/*.hbs']);
-  app.layouts('src/_layouts/*.hbs');
-  app.pages(['src/pages/*.hbs', 'src/pages/**/*.hbs']);
-  app.data({project: config.project});
+  app.partials(["src/_components/**/*.hbs"]);
+  app.layouts("src/_layouts/*.hbs");
+  app.pages(["src/pages/*.hbs", "src/pages/**/*.hbs"]);
+  app.data({ project: config.project });
 
-  return app.toStream('pages', {layout: 'default'})
+  return app
+    .toStream("pages", { layout: "default" })
     .pipe(app.renderFile())
     .pipe(extname())
     .pipe(app.dest(config.paths.dist.html))
@@ -113,40 +109,47 @@ app.task('html', function() {
 
 // JS
 //
-app.task('js', function() {
-  return app.src(config.paths.src.js + 'main.js')
+app.task("js", function () {
+  return app
+    .src(config.paths.src.js + "main.js")
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish', {beep: true}))
+    .pipe(jshint.reporter("jshint-stylish", { beep: true }))
     .pipe(include())
     .pipe(uglify())
-    .pipe(concat(config.project.shortname + '.js'))
+    .pipe(concat(config.project.shortname + ".js"))
     .pipe(app.dest(config.paths.dist.js))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 // Serve
 //
-app.task('serve', function() {
-
+app.task("serve", function () {
   browserSync.init({
     server: {
       baseDir: config.paths.server,
       // directory: true
     },
     notify: false,
-    open: true
+    open: true,
   });
 
-  app.watch(config.paths.src.css, ['sass:main', 'sass:styleguide']);
-  app.watch(config.paths.src.assets, ['assets']);
-  app.watch(config.paths.src.html, ['html']);
-  app.watch(config.paths.src.js, ['js']);
-
+  app.watch(config.paths.src.css, ["sass:main", "sass:styleguide"]);
+  app.watch(config.paths.src.assets, ["assets"]);
+  app.watch(config.paths.src.html, ["html"]);
+  app.watch(config.paths.src.js, ["js"]);
 });
 
 // Default task
 //
-app.task('default', ['clean', 'assets', 'html', 'js', 'sass:main', 'sass:styleguide', 'serve']);
+app.task("default", [
+  "clean",
+  "assets",
+  "html",
+  "js",
+  "sass:main",
+  "sass:styleguide",
+  "serve",
+]);
 
 // expose to assemble's CLI
 //
